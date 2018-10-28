6dc890a9d1d8863c02b300b31698041e569d6b4f
Alexander Busygin <pincher2012@yandex.ru>
2018-10-28 23:03:52 +0500

    All tests passed!

--- services/SeparatorService.php
+++ services/SeparatorService.php
@@ -21,6 +21,38 @@ final class SeparatorService
      */
     public function separate(int $number, array $list): int
     {
-        throw new \Exception('Not implemented');
+        if (count($list) < 2) {
+            return -1;
+        }
+
+        $i = -1;
+        $lastIndex = count($list);
+        $j = $lastIndex;
+
+        while ($i < $j) {
+            do {
+                $i++;
+                if ($i === $lastIndex) {
+                    return -1;
+                }
+
+                if ($i === $j && $list[$i] !== $number) {
+                    return $i;
+                }
+            } while ($list[$i] !== $number);
+
+            do {
+                $j--;
+                if ($j === -1) {
+                    return -1;
+                }
+
+                if ($i === $j && $list[$j] === $number) {
+                    return $i;
+                }
+            } while ($list[$j] === $number);
+        }
+
+        return -1;
     }
 }
